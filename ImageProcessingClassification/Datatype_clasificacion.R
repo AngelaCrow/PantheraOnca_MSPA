09e7e68ab7efe58bd767985f4be89d29a92f0ae5
Angela P. Cuervo-Robayo <ancuervo@gmail.com>
2025-05-02 12:02:11 -0600

    Add files via upload
    Image Processing and Classification in Google Earth Engine

---
+++ ImageProcessingClassification/Datatype_clasificacion.R
@@ -0,0 +1,28 @@
+library(terra)
+getwd() #asegurar que en la ruta estan los archivos exportados de GEE (2 o 3 archivos por cada año analizado) y el shapefile del area de estudio
+
+# Importar a R los raster generados por GEE para el 2018. Estos productos intermedio no se proporcionan. Si los requiere por favor solicitarlos.
+r2018_a <- rast("2018a.tif")  
+r2018_b <- rast("2018b.tif")
+r2018_c <- rast("2018c.tif")
+
+# Importar a R los raster generados por GEE para el 2023.Estos productos intermedio no se proporcionan. Si los requiere por favor solicitarlos.
+r2023_a <- rast("2023a.tif")  
+r2023_b <- rast("2023b.tif") 
+r2023_c <- rast("2023c.tif")  
+
+# Importar area de estudio en formato vector corregido
+aoi <- vect("./Cartografia_metadatos/aoi.shp") #
+
+map2018 <- mosaic(r2018_a,r2018_b,r2018_c)
+map2023 <- mosaic(r2023_a,r2023_b,r2023_c)
+
+cm2018 <- crop(map2018, aoi, mask=TRUE) 
+cm2023 <- crop(map2023, aoi, mask=TRUE)
+
+plot(cm2018)
+plot(cm2023)
+
+#Guardar como INT1U (0-255)
+writeRaster(cm2018, filename = "./2018_Class_utm.tif", datatype = "INT1U")
+writeRaster(cm2023, filename = "./2023_Class_utm.tif", datatype = "INT1U")
